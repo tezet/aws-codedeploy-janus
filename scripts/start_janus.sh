773c1d8c484d0329b9f01268a69e7d7be0a7e07b
Tomasz Zajac <tomasz.zajac@motorolasolutions.com>
2016-06-08 16:07:58 +0200

    Fix start script

--- scripts/start_janus.sh
+++ scripts/start_janus.sh
@@ -2,5 +2,11 @@
 
 PREFIX=/usr
 
+app_pid=`pgrep janus`
+while [[ -n  $(pgrep janus) ]]; do
+    kill $app_pid
+    sleep 1
+done
+
 $PREFIX/share/janus/scripts/janus.cfg.sh
 $PREFIX/bin/janus -b -L /var/log/janus.log
